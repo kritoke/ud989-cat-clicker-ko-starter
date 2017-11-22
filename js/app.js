6a1b6df6689e53ac694074166021b9beb10b87bb
Matt Rhone <kritoke@gmail.com>
2017-11-21 19:27:56 -0600

    Add initial data bindings in html

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var ViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('http://www.fkucjer, ');
+    this.imgAttribution = ko.observable('http://www.flicker.com');
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
