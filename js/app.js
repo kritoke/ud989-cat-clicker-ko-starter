ea33be2679505047c775578bc5755f18feb53f80
Matt Rhone <kritoke@gmail.com>
2017-11-23 15:44:54 -0600

    Add nicknames array and iterate over in data binding

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ var ViewModel = function() {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('http://www.flicker.com');
+    this.nicknames = ko.observableArray([
+        { name: 'Bob' },
+        { name: 'George' }
+    ]);
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
@@ -26,6 +30,7 @@ var ViewModel = function() {
         }
         return levelName;
     }, this);
+
 }
 
 ko.applyBindings(new ViewModel());
